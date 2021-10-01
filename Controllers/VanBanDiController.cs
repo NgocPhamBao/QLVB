264bf66a77f5d1f428b022678b98a46872c712b7
nga1211 <nguyennga12112000@gmail.com>
2021-10-01 09:31:01 +0700

    add Van ban di

---
+++ Controllers/VanBanDiController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLVB.Controllers
+{
+    public class VanBanDiController : Controller
+    {
+        // GET: VanBanDi
+        public ActionResult DangKiVanBanDi()
+        {
+            return View();
+        }
+        public ActionResult VanBanChoPhatHanh()
+        {
+            return View();
+        }
+
+        // GET: VanBanDi/Details/5
+    }
+}
